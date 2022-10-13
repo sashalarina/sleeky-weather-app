2a3c04cc7794ba706ccdde52c224c5ba043ee0df
sashalarina <111566031+sashalarina@users.noreply.github.com>
2022-10-13 15:23:56 +0300

    showTemperature and Forecast via JS API

---
+++ src/script.js
@@ -0,0 +1,22 @@
+function displayForecast(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].main;
+
+  let windElement = document.querySelector("#windspeed");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+}
+
+let apiKey = "7a9230f00d808688d0e6dca4ff6ca11d";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayForecast);
